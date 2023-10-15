afe3a9fa20c76a9e5926f727dbd82cc6ff69e627
Killian <killian.hedou@gmail.com>
2023-10-14 21:07:42 -0400

    Added basic coloring and formatting

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ function App() {
   return (
     <div className="App">
         <h2 className='header'>MusicMerger</h2>
-        <p>
-          A tool for converting spotify playlists to!
+        <p className='description'>
+          A tool for converting spotify playlists to youtube playlists!
         </p>
+        <div className='input-container'>
+          <input className='spotify-input' type='text' placeholder='Spotify Playlist URL' />
+          <button className='convert-button'>Convert</button>
+        </div>
 
     </div>
   );
