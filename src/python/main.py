5e29d4753897e5f1a64596aa280d50a4c577f22b
Alexandre Hachey <alx99102@gmail.com>
2023-10-14 21:15:31 -0400

    added files

---
+++ src/python/main.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import get_playlist
+import create_playlist
+
+
+
+URL = "https://api.musicapi.com/public/search"
+
+
+
+
+payload = json.dumps({
+  "track": "Bezos I",
+  "type": "track",
+  "sources": [
+    "spotify",
+    "youtube",
+  ]
+})
+headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': 'Token 9612173b-0afb-45ce-aa05-4dc065481d94'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
