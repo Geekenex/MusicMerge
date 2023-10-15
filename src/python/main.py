0aa285de47a712823aca31c8de469d4b7636fcee
Alexandre Hachey <alx99102@gmail.com>
2023-10-14 21:16:11 -0400

    fixed mistake in main

--- src/python/main.py
+++ src/python/main.py
@@ -8,8 +8,6 @@ import create_playlist
 URL = "https://api.musicapi.com/public/search"
 
 
-
-
 payload = json.dumps({
   "track": "Bezos I",
   "type": "track",
@@ -24,6 +22,6 @@ headers = {
   'Authorization': 'Token 9612173b-0afb-45ce-aa05-4dc065481d94'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+response = requests.request("POST", URL, headers=headers, data=payload)
 
 print(response.text)
