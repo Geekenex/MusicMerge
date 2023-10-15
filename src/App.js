c0e85cfe642f6f4d830873b88f0c85ac41f3d068
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2023-10-14 21:30:24 -0400

    started adding url validation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const playlistURL = useRef('');
+
+  function handleURLInput(e){
+    console.log(playlistURL.current.value);
+
+    if (playlistURL.current.value.startsWith("https://open.spotify.com/playlist")){
+      console.log("do a url request");
+    }
+    else
+      console.log("throw an error");
+  }
+
+
   return (
     <div className="App">
         <h2 className='header'>MusicMerge</h2>
-        <p>
+        <p className='description'>  
           A tool for converting spotify playlists to!
         </p>
         <div className='input-container'>
-          <input className='spotify-input' type='text' placeholder='Spotify Playlist URL' />
-          <button className='convert-button'>Convert</button>
+          <input className='spotify-input' type='text' ref={playlistURL} placeholder='Spotify Playlist URL' />
+          <button className='convert-button' onClick={handleURLInput}>Convert</button>
         </div>
 
     </div>
