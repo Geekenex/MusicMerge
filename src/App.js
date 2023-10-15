bc4513696c67067fb0c3174a8e1db8eab9e6ee44
Killian <killian.hedou@gmail.com>
2023-10-14 20:57:22 -0400

    Added base layout for front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2 className='header'>MusicMerger</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A tool for converting spotify playlists to .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
