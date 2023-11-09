f018dff895c157a919e8cb3bd0a10f2992f08a83
Killian Hedou <killian.hedou@gmail.com>
2023-11-08 21:07:14 -0500

    still no work

--- src/PlaylistOut.js
+++ src/PlaylistOut.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 function PlaylistOut({ url }) {
-  const [playlistInfo, setPlaylistInfo] = useState({});
+  const [playlistInfo, setPlaylistInfo] = useState({url, tracks: [] });
   const [error, setError] = useState("");
 
   useEffect(() => {
@@ -33,20 +33,26 @@ function PlaylistOut({ url }) {
     apiCall();
   }, [url]);
 
-  if(!playlistInfo) {
+  if(Object.keys(playlistInfo).length === 0) {
     return <div className='output-container'>Loading converted playlist info...</div>;
   }
-
   return (
     <div className='output-container'>
-      <p className='output'>Converted playlist: {playlistInfo.url}</p>
-      {playlistInfo.tracks ? (
-        <ul>
-          {playlistInfo.tracks.map((track, index) => (
-            <li key={index}><img src={track.image} alt={track.name} />Name: {track.name}</li>
-          ))}
-        </ul>
-      ) : null}
+      {error && <p>Error: {error}</p>}
+      {playlistInfo.url ? (
+        <>
+          <p className='output'>Converted playlist: {playlistInfo.url}</p>
+          {playlistInfo.tracks.length > 0 ? (
+            <ul>
+              {playlistInfo.tracks.map((track, index) => (
+                <li key={index}><img src={track.image} alt={track.name} />Name: {track.name}</li>
+              ))}
+            </ul>
+          ) : <div>No tracks found.</div>}
+        </>
+      ) : (
+        <div>Loading converted playlist info...</div>
+      )}
     </div>
   );
 }
